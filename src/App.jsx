c130863f60678475bd70217e021dc51d5d37e9af
fserakib26 <fserakib26@gmail.com>
2026-03-31 15:32:30 +0600

    NavBar Design Completed and React Icons has been installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react"
+import NavBar from "./components/NavBar"
 
 function App() {
   
   return (
     <>
-      <h2 class="text-9xl text-center">Hello</h2>
-      <button class="btn btn-primary">DaisiUI</button>
+      <NavBar/>
     </>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { CiShoppingCart } from 'react-icons/ci'
+
+const NavBar = () => {
+  return (
+    <div className="max-w-7xl mx-auto">
+        <div className="navbar">
+            <div className="navbar-start">
+                <div className="dropdown">
+                <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                </div>
+                <ul
+                    tabIndex="-1"
+                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                    <li><a>Products</a></li>            
+                    <li><a>Features</a></li>
+                    <li><a>Pricing</a></li>
+                    <li><a>Testimonials</a></li>
+                    <li><a>FAQ</a></li>
+
+                </ul>
+                </div>
+                <a className="text-4xl font-bold bg-gradient-to-r from-[#4f39f6] to-[#9514fa] bg-clip-text text-transparent">DigiTools</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Products</a></li>            
+                    <li><a>Features</a></li>
+                    <li><a>Pricing</a></li>
+                    <li><a>Testimonials</a></li>
+                    <li><a>FAQ</a></li>
+                </ul>
+            </div>
+            <div className="navbar-end space-x-5">
+                <a><CiShoppingCart/></a>
+                <a>Login</a>
+                <a className="btn bg-gradient-to-r from-[#4f39f6] to-[#9514fa] rounded-full text-white">Get Started</a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
